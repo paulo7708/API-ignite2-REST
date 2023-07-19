f7035ad8d8597782c10595cdb0e04364dfcad8c9
paulo7708 <paulohenrique7708@hotmail.com>
2023-07-19 14:48:52 -0300

    Tipagem no Knex

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import Fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = Fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions').select('*')
-  // const transaction = await knex('transactions').insert({
-  //   id: crypto.randomUUID(),
-  //   title: 'Transação de teste',
-  //   amount: 1000,
-  // }).returning('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  //prefix: 'transactions'
 })
 
 app
